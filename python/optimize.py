a739ffb63356729a276f1cdd79677814d7aba0b0
Durga <durgamohan96@gmail.com>
2018-03-07 17:24:09 +0530

    applied profiling and graph

--- python/optimize.py
+++ python/optimize.py
@@ -1,3 +1,7 @@
 import cProfile
 import re
+from pympler import tracker
+tr = tracker.SummaryTracker()
 cProfile.run('re.compile("foo|bar")', 'restats')
+
+tr.print_diff()
--- python/with_for.py
+++ python/with_for.py
@@ -2,18 +2,8 @@ import json
 from functools import reduce
 
 def get_total_bytes(src, data):
-    total[src] = reduce(
-        lambda x, y: x + y,
-        list(
-            map(
-                lambda data: data["total_bytes"],
-                filter(
-                    lambda data: data["src_ip"] == src,
-                    data
-                )
-            )
-        )
-    )
+    l = [x["total_bytes"] for x in data if x["src_ip"] == src]
+    total[src] = reduce(lambda x, y: x + y, l)
 
 data = json.load(open("sample.json"))
 data = data["data"]
